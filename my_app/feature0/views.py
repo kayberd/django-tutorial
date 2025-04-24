a597899d7b48a9bf7955e89bba75011c49ab8787
Berkay Demirören <berkay.demiroeren@petleo.net>
2025-04-24 08:45:40 +0300

    Add counter view and URL, update index view to render template, and configure template directory in settings

--- my_app/feature0/views.py
+++ my_app/feature0/views.py
@@ -2,4 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('<h1> Hey, welcome')
+    return render(request, 'index.html')
+
+def counter(request):
+    text = request.POST['text']
+    num_of_words = len(text.split())
+    context = {'num_of_words': num_of_words}
+    return render(request, 'counter.html', context)
