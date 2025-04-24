d1ac3b3e70f21b83831697c1b861dee3f87e1796
Berkay Demirören <berkay.demiroeren@petleo.net>
2025-04-24 11:41:38 +0300

    Add feature model

--- my_app/feature0/views.py
+++ my_app/feature0/views.py
@@ -1,8 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . models import Feature
 
 def index(request):
-    return render(request, 'index.html')
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'Our service is very quick'
+    feature1.is_true = True
+    
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Fast 2'
+    feature2.details = 'Our service is very quick 2'
+    feature2.is_true = False
+    
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Fast 3'
+    feature3.details = 'Our service is very quick 3 '
+    feature3.is_true = True
+    
+    featureList = [feature1, feature2, feature3]
+    context = {
+        'features': featureList
+    }
+    return render(request, 'index.html', context)
 
 def counter(request):
     text = request.POST['text']
