9f6bc2ac5b8407163c3bffb338e3e33ef67f0973
Berkay Demirören <berkay.demiroeren@petleo.net>
2025-04-28 00:11:30 +0300

    Learn adding models into db

--- my_app/feature0/views.py
+++ my_app/feature0/views.py
@@ -3,27 +3,10 @@ from django.shortcuts import render
 from . models import Feature
 
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Fast'
-    feature1.details = 'Our service is very quick'
-    feature1.is_true = True
-    
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Fast 2'
-    feature2.details = 'Our service is very quick 2'
-    feature2.is_true = False
-    
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Fast 3'
-    feature3.details = 'Our service is very quick 3 '
-    feature3.is_true = True
-    
-    featureList = [feature1, feature2, feature3]
+    features = Feature.objects.all()
+
     context = {
-        'features': featureList
+        'features': features
     }
     return render(request, 'index.html', context)
 
